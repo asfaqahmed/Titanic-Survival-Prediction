93da2c1e496e41cd9ecaa3291366c64e0fb0882c
Asfaq Ahmed <asfaqahmed356@gmail.com>
2024-08-18 12:15:58 +0530

    Titanic_Survival_Prediction

---
+++ scripts/titanic_model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load the data
+train_df = pd.read_csv('../data/train.csv')
+test_df = pd.read_csv('../data/test.csv')
+
+# Explore the data
+# print(train_df.head())
+# print(train_df.info())
+# print(train_df.describe())
